fd7c4f86826f4bfa10b350c4a70ababf0f7164f6
Tenzin Lekphell <61218333+tenzinlekphell@users.noreply.github.com>
2022-08-08 22:22:27 -0400

    Able to add blog message to user interface

--- src/blogComponents/BlogCreatePost.jsx
+++ src/blogComponents/BlogCreatePost.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogCreatePost = () => {
+  const [messageInput, setMessageInput] = useState('');
+  const [renderMessage, setRenderMessage] = useState([]);
+
+  const messageHandler = (event) => {
+    setMessageInput(event.target.value);
+  };
+
+  const addMessage = () => {
+    setRenderMessage([...renderMessage, messageInput]);
+  };
+
   return (
     <div>
       <h1 className="newPostHeader">New Post</h1>
@@ -8,8 +19,19 @@ const BlogCreatePost = () => {
         <label htmlFor="">Blog Title</label>
         <input type="text" placeholder="Enter a title" />
         <label htmlFor="">Blog Body</label>
-        <input type="text" placeholder="Start writing here" />
-        <button>Submit</button>
+        <input
+          type="text"
+          placeholder="Start writing here"
+          onChange={messageHandler}
+        />
+        <button onClick={addMessage}>Submit</button>
+        {renderMessage.map((message) => {
+          return (
+            <div>
+              <li>{message}</li>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
